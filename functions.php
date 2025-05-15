40597cb857c3938eca333b74c48dceb721a6e617
Petar Petrov <32793855+pdpetrov98@users.noreply.github.com>
2025-05-15 13:54:04 +0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,8 +1,8 @@
 <?php
 
-function oxy_minimum_enqueue_scripts() {
+function cb_oxy_minimum_enqueue_scripts() {
     // Load main stylesheet
     wp_enqueue_style( 'style', get_stylesheet_uri() );
     
 }
-add_action( 'wp_enqueue_scripts', 'oxy_minimum_enqueue_scripts' );
+add_action( 'wp_enqueue_scripts', 'cb_oxy_minimum_enqueue_scripts' );
